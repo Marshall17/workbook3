b9149e6d95fb1cc257ee9270ce012c6620c103e4
Marshal Rai <kusmet722@gmail.com>
2023-05-01 11:42:39 -0400

    exersices

---
+++ exercises/simple_functions.js
@@ -0,0 +1,18 @@
+function favoriteThings() {
+    console.log("My name is Mars, I enjoy Gintama and Camilia Cabello.");
+    
+}
+favoriteThings()
+
+function whyImHere() {
+    console.log(
+        `I wanted to learn coding for making games website
+and just my intrest on the topic`);
+    
+}
+whyImHere()
+
+function favortiePlace() {
+    console.log("I enjoyed visiting North Carolina because of volleyball");
+}
+favortiePlace()
